6688971aac4ebcda829496f8d0c5ea1a6b0f21d4
Kumar Shashwat <kumarshashwat62@gmail.com>
2018-09-12 15:48:56 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/* 
+	author: "Kumar Shashwat"
+	Company: "DevIt consultancy
+	Website: "https://devitconsultancy.com/#services"
+	Subject to Copyright
+*/
+
+
+//Scroll Reveal
+window.sr = ScrollReveal();
+sr.reveal('.intro1', {
+  origin: 'left',
+  distance: '100px',
+  delay: 500,
+  duration: 1500
+
+});
+sr.reveal('.intro2', {
+  origin: 'bottom',
+  distance: '100px',
+  delay: 1000,
+  duration: 1500
+
+});
+
+
+// CounterUp
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
+
+
+/* 
+	author: "Kumar Shashwat"
+	Company: "DevIt consultancy
+	Website: "https://devitconsultancy.com/#services"
+	Subject to Copyright
+*/
